817816fe2cc37446aa05d3cd679084cf19372f86
Viklysx <vikbelec111@yandex.ru>
2021-05-08 20:41:16 +0300

    feat: added an error message

--- app/src/components/ColorConverter.js
+++ app/src/components/ColorConverter.js
@@ -10,13 +10,18 @@ export default function ColorConverter({ props }) {
     const handleColorChange = evt => {
         const valueColor = evt.target.value;
         const reg = /#[a-f0-9]{6}/gi;
-        if (valueColor.length === 7 && reg.test(valueColor)) {         
-            const r = parseInt(valueColor.substring(1,3),16);
-            const g = parseInt(valueColor.substring(3,5),16);
-            const b = parseInt(valueColor.substring(5),16);
-            const rgb = `rgb(${r}, ${g}, ${b})`;
-            setForm(prevForm => ({...prevForm, hex: evt.target.value, rgb: rgb}));
-        }
+        setForm(prevForm => ({...prevForm, rgb: ''}));
+        if (valueColor.length === 7) {   
+            if (reg.test(valueColor)) {
+                const r = parseInt(valueColor.substring(1,3),16);
+                const g = parseInt(valueColor.substring(3,5),16);
+                const b = parseInt(valueColor.substring(5),16);
+                const rgb = `rgb(${r}, ${g}, ${b})`;
+                setForm(prevForm => ({...prevForm, hex: evt.target.value, rgb: rgb}));
+            } else {
+                setForm(prevForm => ({...prevForm, hex: evt.target.value, rgb: 'Ошибка!'}));
+            }            
+        } 
     };
 
     return ( 
